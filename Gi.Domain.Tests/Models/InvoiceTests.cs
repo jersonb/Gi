7c3db321ed418f1731c90953ab8c5f38cc4b29ba
jersonb <jersonlbrito@hotmail.com>
2022-09-09 21:08:02 -0300

    feat: move classes to specific files

--- Gi.Domain.Tests/Models/InvoiceTests.cs
+++ Gi.Domain.Tests/Models/InvoiceTests.cs
@@ -28,72 +28,22 @@ public class InvoiceTests
 
         var lines = readedLines.Select(l => new Line(l));
         var invoiceLines = lines.Where(line => line.Register == RegisterName._8530 || line.Register == RegisterName._8535).Reverse();
-        
+
         var invoiceItemsLines = new List<Line>();
         var invoices = new List<Invoice>();
         foreach (var line in invoiceLines)
         {
-            if(line.Register == RegisterName._8530)
+            if (line.Register == RegisterName._8530)
             {
                 var invoice = new Invoice(line, invoiceLines);
                 invoices.Add(invoice);
                 invoiceItemsLines = new List<Line>();
-            }else
+            }
+            else
                 invoiceItemsLines.Add(line);
         }
         Assert.Equal(17, lines.Count());
         Assert.Equal(12, invoiceLines.Count());
         Assert.Equal(4, invoices.Count);
-        var items = from invoice in invoices
-                from item in invoice.InvoiceItems
-                select item;
-        Assert.Equal(8, items.Count());
-        
     }
-}
-public class Invoice
-{
-    public Invoice(Line line, IEnumerable<Line> lines)
-    {
-        if(line.Register != RegisterName._8530)
-            throw new Exception("foi aq 1");
-
-        if(!lines.Any(l => l.Register != RegisterName._8535))
-            throw new Exception("foi aq 2");
-
-        Number = line.Content[6];
-        InvoiceItems = lines.Select(l => new InvoiceItem(l));
-    }
-    public string Number { get; set; }
-    public IEnumerable<InvoiceItem> InvoiceItems { get; set; }
-}
-public class InvoiceItem
-{
-    public InvoiceItem(Line line)
-    {
-         if(line.Register != RegisterName._8535)
-            throw new Exception("foi aqui 3");
-
-        ItemCode = line.Content[2];
-        Cfop = line.Content[3];
-        AmmountString = line.Content[4];
-        
-        if(!string.IsNullOrEmpty(AmmountString))
-            Ammount = decimal.Parse(AmmountString);
-        
-        Aliquot = line.Content[5];
-
-        IcmsString = line.Content[6];
-       
-        if (!string.IsNullOrEmpty(IcmsString))
-            Icms = decimal.Parse(IcmsString);
-    }
-
-    public string ItemCode { get; }
-    public string Cfop { get; }
-    public string AmmountString { get; }
-    public decimal Ammount { get; }
-    public string Aliquot { get; }
-    public string IcmsString { get; }
-    public decimal Icms { get; }
 }
